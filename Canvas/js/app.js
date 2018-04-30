e3f71ff50657bf0597c44ddf41666b91e3dc0a60
Michael Smith <michaelasmith1991@gmail.com>
2018-04-30 16:10:06 +0100

    Added collision and fighting logic

--- Canvas/js/app.js
+++ Canvas/js/app.js
@@ -14,6 +14,17 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function valueInRange(value, min, max) {
+  return (value <= max) && (value >= min);
+}
+
+//Game funcs
+function checkElementsAreOverlapping(A, B) {
+  const xOverlap = valueInRange(A.x, B.x, B.x + B.width) || valueInRange(B.x, A.x, A.x + A.width);
+  const yOverlap = valueInRange(A.y, B.y, B.y + B.height) || valueInRange(B.y, A.y, A.y + A.height);
+  return xOverlap && yOverlap;
+}
+
 function pickRandomDirection() {
   let i = randomInt(1, 3);
   let direction = null;
@@ -27,21 +38,6 @@ function pickRandomDirection() {
   return direction;
 }
 
-function valueInRange(value, min, max) {
-  return (value <= max) && (value >= min);
-}
-
-function checkRectanglesAreOverlapping(A, B) {
-  const xOverlap = valueInRange(A.x, B.x, B.x + B.width) ||
-    valueInRange(B.x, A.x, A.x + A.width);
-
-  const yOverlap = valueInRange(A.y, B.y, B.y + B.height) ||
-    valueInRange(B.y, A.y, A.y + A.height);
-
-  return xOverlap && yOverlap;
-}
-
-//Game funcs
 function addMultipleEntitiesToAnArray(containerArray, amount, name, colour, speed, health, damage, sight, stealth, fertilityRate) {
   for (i = 0; i < amount; i++) {
     let xDirection = pickRandomDirection();
@@ -66,7 +62,7 @@ function addMultipleEntitiesToAnArray(containerArray, amount, name, colour, spee
     const width = 20;
     for (let arrayEntity of containerArray) {
       const rectA = { x: x, y: y, height: height, width: width };
-      const isOverlapping = checkRectanglesAreOverlapping(rectA, arrayEntity);
+      const isOverlapping = checkElementsAreOverlapping(rectA, arrayEntity);
       if (isOverlapping) {
         //console.log("Entity", rectA, "overlapping Entity:", arrayEntity);
         //TODO: Find better way of moving without potentially moving into the place of another entity
@@ -96,7 +92,8 @@ function addMultipleEntitiesToAnArray(containerArray, amount, name, colour, spee
       sight: sight,
       stealth: stealth,
       fertile: true,
-      fertilityRate: fertilityRate
+      fertilityRate: fertilityRate,
+      isDead: false
     }
     containerArray.push(entity);
     //console.log("createObject() entity =", entity);
@@ -106,9 +103,68 @@ function addMultipleEntitiesToAnArray(containerArray, amount, name, colour, spee
 
 function drawObjects(canvasContext, containerArray) {
   //console.log("Drawing", containerArray);
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   for (i = 0; i < containerArray.length; i++) {
     canvasContext.fillStyle = containerArray[i].colour;
     canvasContext.fillRect(containerArray[i].x, containerArray[i].y, containerArray[i].width, containerArray[i].height);
+  }
+}
+
+function reverseElementIfOnSceneBoundaries(element) {
+  if (element.x === 1 || element.x + element.width === scene.width) {
+    element.vx *= -1;
+  }
+  if (element.y === 1 || element.y + element.height === scene.height) {
+    element.vy *= -1;
+  }
+}
+
+function killElementWithNoHealth(element) {
+  if (element.health === 0) {
+    element.isDead = true;
+  }
+}
+
+function validTarget(elementA, elementB) {
+  let valid = false;
+  if (((elementA.name === "prey" && elementB.name === "plant") || (elementA.name === "plant" && elementB.name === "prey")) ||
+    ((elementA.name === "prey" && elementB.name === "predator") || (elementA.name === "predator" && elementB.name === "prey"))) {
+    valid = true;
+  }
+  return valid;
+}
+
+function fighting(elementA, elementB) {
+  if (elementA.health > 0 && elementB.health > 0) {
+    elementA.vx = 0;
+    elementA.vy = 0;
+    elementB.vx = 0;
+    elementB.vy = 0;
+    elementA.health -= elementB.damage;
+    elementB.health -= elementA.damage;
+  }
+  killElementWithNoHealth(elementA);
+  killElementWithNoHealth(elementB);
+}
+
+function moveElementsInOppositeDirections(elementA, elementB) {
+  //TODO: calculate direction hit and move each element in opposite directions
+}
+
+function moveElementAtCurrentVelocity(element) {
+  element.x += element.vx;
+  element.y += element.vy;
+}
+
+function checkElementIsInContactWithAnother(element) {
+  for (let organism of organismsArray) {
+    if (checkElementsAreOverlapping(element, organism)) {
+      if (validTarget(element, organism)) {
+        fighting(element, organism);
+      } else {
+        moveElementsInOppositeDirections(element, organism);
+      }
+    }
   }
 }
 
@@ -116,7 +172,7 @@ const mouseUpHandler = function (event) {
   const x = event.pageX - canvas.offsetLeft;
   const y = event.pageY - canvas.offsetTop;
   //console.log("Click! x", event.pageX, "y", event.pageY);
-  for (element of organismsArray) {
+  for (let element of organismsArray) {
     // console.log(x ,">=", element.x, "&&", x, "<=", element.x, "+", element.width, "&&", y, ">=", element.y, "&&", y, "<=", element.y, "+", element.height,
     // x >= element.x && x <= element.x + element.width && y >= element.y && y <= element.y + element.height);
     if (x >= element.x && x <= element.x + element.width && y >= element.y && y <= element.y + element.height) {
@@ -137,11 +193,12 @@ function init() {
 
 function draw() {
   //console.log("Running Draw()");  
-  if (canvas.getContext) {        
+  if (canvas.getContext) {
     drawObjects(ctx, organismsArray);
-    for (element of organismsArray) {
-      element.x += element.vx;
-      element.y += element.vy;
+    for (let element of organismsArray) {
+      reverseElementIfOnSceneBoundaries(element);
+      moveElementAtCurrentVelocity(element);
+      checkElementIsInContactWithAnother(element);
     }
     canvas.addEventListener('mouseup', mouseUpHandler, false);
     window.requestAnimationFrame(draw);
